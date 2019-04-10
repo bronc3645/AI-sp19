e5607534eb06145acd9ab60a9ad4a8cb7aef8e44
bronc3645 <42680121+bronc3645@users.noreply.github.com>
2019-04-10 15:47:35 -0600

    Add files via upload
    this is a version that has been cleaned up a tiny bit and made so it doesn't go to -1000 or a suicide run

--- PyAgent.py
+++ PyAgent.py
@@ -9,24 +9,23 @@ class Agent:
     def __init__(self):
         self.agentHasGold = False
         self.actionList = []
-        self.lotovisit=[]
-        self.locvisted=[]
-        self.frontier=[]
-        self.known=[]
+        self.returnList=[]
+        self.tovisit=[]
+        self.visited=[]
         self.searchEngine = Search.SearchEngine()
         self.oritation=Orientation.RIGHT
         self.posx=1
         self.posy=1
         self.wumpus=True
         self.pos=[]
+
     def Initialize(self):
         # Works only for test world.
         # You won't initially know safe locations or world size.
         self.searchEngine.AddSafeLocation(1,1)
         self.agentHasGold = False
         self.actionList = []
-        self.locvisted.append([1,1])
-        self.known.append([1,1,0,0])
+        self.visited.append([1,1])
 
     # Input percept is a dictionary [perceptName: boolean]
     def Process(self, stench, breeze, glitter, bump, scream):
@@ -47,95 +46,111 @@ class Agent:
                 if(not self.posy==4):
                     self.posy+=1
         if (bool(glitter)):
-            self.actionList.append(Action.GRAB)
+            self.returnList=self.searchEngine.FindPath([self.posx,self.posy],self.oritation,[1,1],self.oritation)
+            self.returnList.append(Action.CLIMB)
+            return Action.GRAB
+        if(self.returnList):
+            action=self.returnList.pop(0)
+            self.track(action)
+            return action
         elif (not self.actionList):
-            if (not self.agentHasGold):
-                if(bool(breeze)):
-                    if(not [self.posx+1,self.posy] in self.locvisted and self.posx+1<=5):
-                        self.searchEngine.RemoveSafeLocation(self.posx+1,self.posy)
-                    if(not [self.posx,self.posy+1] in self.locvisted and self.posy+1<=5):
-                        self.searchEngine.RemoveSafeLocation(self.posx, self.posy+1)
-                    if(not [self.posx-1,self.posy] in self.locvisted and self.posx-1>=1):
-                        self.searchEngine.RemoveSafeLocation(self.posx-1, self.posy)
-                    if(not [self.posx,self.posy-1] in self.locvisted and self.posy-1>=1):
-                        self.searchEngine.RemoveSafeLocation(self.posx, self.posy-1)
-                elif(bool(stench)):
-                    if (not [self.posx + 1, self.posy] in self.locvisted and self.posx + 1 <= 5):
-                        self.searchEngine.RemoveSafeLocation(self.posx + 1, self.posy)
-                    if (not [self.posx, self.posy + 1] in self.locvisted and self.posy + 1 <= 5):
-                        self.searchEngine.RemoveSafeLocation(self.posx, self.posy + 1)
-                    if (not [self.posx - 1, self.posy] in self.locvisted and self.posx - 1 >= 1):
-                        self.searchEngine.RemoveSafeLocation(self.posx - 1, self.posy)
-                    if (not [self.posx, self.posy - 1] in self.locvisted and self.posy - 1 >= 1):
-                        self.searchEngine.RemoveSafeLocation(self.posx, self.posy - 1)
-                else:
-                    if (not [self.posx + 1, self.posy] in self.searchEngine.explored and self.posx+1<=4):
-                        self.searchEngine.AddSafeLocation(self.posx+1,self.posy)
-                        self.lotovisit.append([self.posx+1,self.posy])
-                    if (not [self.posx - 1, self.posy] in self.searchEngine.explored and self.posx-1>=1):
-                        self.searchEngine.AddSafeLocation(self.posx-1,self.posy)
-                        self.lotovisit.append([self.posx - 1, self.posy])
-                    if (not [self.posx, self.posy+1] in self.searchEngine.explored and self.posy+1<=4):
-                        self.searchEngine.AddSafeLocation(self.posx,self.posy+1)
-                        self.lotovisit.append([self.posx, self.posy+1])
-                    if (not [self.posx, self.posy-1] in self.searchEngine.explored and self.posy-1>=1):
-                        self.searchEngine.AddSafeLocation(self.posx,self.posy-1)
-                        self.lotovisit.append([self.posx, self.posy-1])
-                if(bool(glitter)):
-                    self.actionList.clear()
-                    self.actionList.append(Action.GRAB)
-                elif(self.lotovisit):
-                    self.pos=self.lotovisit.pop(0)
-                    self.actionList+=self.searchEngine.FindPath([self.posx,self.posy],self.oritation,self.pos,self.oritation)
-                else:
-                    self.actionList+=self.searchEngine.FindPath([self.posx,self.posy],self.oritation,[1,1],self.oritation)
-                    self.actionList.append(Action.CLIMB)
-                if(not self.actionList):
-                    self.actionList.append(Action.GOFORWARD)
+            if(bool(breeze)):
+                if((not [self.posx+1,self.posy] in self.visited) and self.posx+1<=5):
+                    self.searchEngine.RemoveSafeLocation(self.posx+1,self.posy)
+                if((not [self.posx,self.posy+1] in self.visited) and self.posy+1<=5):
+                    self.searchEngine.RemoveSafeLocation(self.posx, self.posy+1)
+                if((not [self.posx-1,self.posy] in self.visited) and self.posx-1>=1):
+                    self.searchEngine.RemoveSafeLocation(self.posx-1, self.posy)
+                if((not [self.posx,self.posy-1] in self.visited) and self.posy-1>=1):
+                    self.searchEngine.RemoveSafeLocation(self.posx, self.posy-1)
+            elif(bool(stench)):
+                if (not [self.posx + 1, self.posy] in self.visited and self.posx + 1 <= 5):
+                    self.searchEngine.RemoveSafeLocation(self.posx + 1, self.posy)
+                if (not [self.posx, self.posy + 1] in self.visited and self.posy + 1 <= 5):
+                    self.searchEngine.RemoveSafeLocation(self.posx, self.posy + 1)
+                if (not [self.posx - 1, self.posy] in self.visited and self.posx - 1 >= 1):
+                    self.searchEngine.RemoveSafeLocation(self.posx - 1, self.posy)
+                if (not [self.posx, self.posy - 1] in self.visited and self.posy - 1 >= 1):
+                    self.searchEngine.RemoveSafeLocation(self.posx, self.posy - 1)
             else:
-                self.actionList += self.searchEngine.FindPath([self.posx,self.posy], self.oritation, [1, 1], Orientation.RIGHT)
+                if (not [self.posx + 1, self.posy] in self.searchEngine.explored and self.posx+1<=4):
+                    self.searchEngine.AddSafeLocation(self.posx+1,self.posy)
+                    if(not [self.posx+1,self.posy]in self.tovisit):
+                        self.tovisit.append([self.posx + 1, self.posy])
+                if (not [self.posx - 1, self.posy] in self.searchEngine.explored and self.posx-1>=1):
+                    self.searchEngine.AddSafeLocation(self.posx-1,self.posy)
+                    if (not [self.posx - 1, self.posy] in self.tovisit):
+                        self.tovisit.append([self.posx - 1, self.posy])
+                if (not [self.posx, self.posy+1] in self.searchEngine.explored and self.posy+1<=4):
+                    self.searchEngine.AddSafeLocation(self.posx,self.posy+1)
+                    if (not [self.posx, self.posy+1] in self.tovisit):
+                        self.tovisit.append([self.posx, self.posy+1])
+                if (not [self.posx, self.posy-1] in self.searchEngine.explored and self.posy-1>=1):
+                    self.searchEngine.AddSafeLocation(self.posx,self.posy-1)
+                    if (not [self.posx, self.posy-1] in self.tovisit):
+                        self.tovisit.append([self.posx, self.posy-1])
+            if(self.tovisit):
+                self.pos=self.tovisit.pop(0)
+                self.actionList+=self.searchEngine.FindPath([self.posx,self.posy],self.oritation,self.pos,self.oritation)
+            else:
+                self.pos=[1,1]
+                self.actionList+=self.searchEngine.FindPath([self.posx,self.posy],self.oritation,[1,1],self.oritation)
                 self.actionList.append(Action.CLIMB)
         print(str(self.pos))
         print(str(self.posx)+":x")
         print(str(self.posy)+":y")
         print(str(self.oritation)+":oritation")
         self.printworld()
-        action = self.actionList.pop(0)
-        if(action==Action.GOFORWARD):
-            if(self.oritation==Orientation.RIGHT):
-                self.posx+=1
-            elif(self.oritation==Orientation.LEFT):
-                self.posx-=1
-            elif(self.oritation==Orientation.UP):
-                self.posy+=1
-            elif(self.oritation==Orientation.DOWN):
-                self.posy-=1
-        if(action==Action.TURNRIGHT):
-            if (self.oritation == Orientation.RIGHT):
-                self.oritation=Orientation.DOWN
-            elif (self.oritation == Orientation.LEFT):
-                self.oritation=Orientation.UP
-            elif (self.oritation == Orientation.UP):
-                self.oritation=Orientation.RIGHT
-            elif (self.oritation == Orientation.DOWN):
-                self.oritation=Orientation.LEFT
-        if(action==Action.TURNLEFT):
-            if (self.oritation == Orientation.RIGHT):
-                self.oritation=Orientation.UP
-            elif (self.oritation == Orientation.LEFT):
-                self.oritation=Orientation.DOWN
-            elif (self.oritation == Orientation.UP):
-                self.oritation=Orientation.LEFT
-            elif (self.oritation == Orientation.DOWN):
-                self.oritation=Orientation.RIGHT
-        if(action==Action.GRAB):
-            self.agentHasGold=True
-        if(not[self.posx,self.posy]in self.locvisted):
-            self.locvisted.append([self.posx,self.posy])
+        if(self.actionList):
+            action = self.actionList.pop(0)
+        else:
+            self.actionList+=self.searchEngine.FindPath([self.posx,self.posy],self.oritation,[1,1],self.oritation)
+            self.actionList.append(Action.CLIMB)
+            action=self.actionList.pop(0)
+        self.track(action)
+        if(not[self.posx,self.posy]in self.visited):
+            self.visited.append([self.posx,self.posy])
+        self.check();
         return action
     def printworld(self):
-        for x in self.locvisted:
+        print("visited")
+        for x in self.visited:
             print(str(x))
+        print("to visit")
+        for x in self.tovisit:
+            print(str(x))
+    def check(self):
+        for x in self.tovisit:
+            if(x in self.visited):
+                self.tovisit.remove(x)
+    def track(self,action):
+        if (action == Action.GOFORWARD):
+            if (self.oritation == Orientation.RIGHT):
+                self.posx += 1
+            elif (self.oritation == Orientation.LEFT):
+                self.posx -= 1
+            elif (self.oritation == Orientation.UP):
+                self.posy += 1
+            elif (self.oritation == Orientation.DOWN):
+                self.posy -= 1
+        if (action == Action.TURNRIGHT):
+            if (self.oritation == Orientation.RIGHT):
+                self.oritation = Orientation.DOWN
+            elif (self.oritation == Orientation.LEFT):
+                self.oritation = Orientation.UP
+            elif (self.oritation == Orientation.UP):
+                self.oritation = Orientation.RIGHT
+            elif (self.oritation == Orientation.DOWN):
+                self.oritation = Orientation.LEFT
+        if (action == Action.TURNLEFT):
+            if (self.oritation == Orientation.RIGHT):
+                self.oritation = Orientation.UP
+            elif (self.oritation == Orientation.LEFT):
+                self.oritation = Orientation.DOWN
+            elif (self.oritation == Orientation.UP):
+                self.oritation = Orientation.LEFT
+            elif (self.oritation == Orientation.DOWN):
+                self.oritation = Orientation.RIGHT
     def GameOver(self, score):
         pass
 
